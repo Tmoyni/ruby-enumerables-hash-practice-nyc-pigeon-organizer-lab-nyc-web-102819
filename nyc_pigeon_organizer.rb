ca5276da4ab52850493012f1511a158a768729a2
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 05:27:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
  new_hash = {}
   data.each do |key, value|
-    value.each do |new_value, name|
+    value.each do |new_value, names|
       element.each do |name|
         
         if !new_hash[name]
