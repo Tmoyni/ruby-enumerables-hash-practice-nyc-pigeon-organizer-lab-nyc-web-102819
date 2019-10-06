bc48f15ad47babc1450b0ee5f02585695b4d583f
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 05:28:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,21 @@
 def nyc_pigeon_organizer(data)
- new_hash = {}
+  new_hash = {}
   data.each do |key, value|
     value.each do |new_value, names|
-      element.each do |name|
+      names.each do |name|
         
         if !new_hash[name]
           new_hash[name] = {}
-        end   
-        
+        end
+
         if !new_hash[name][key]
           new_hash[name][key] = []
-        end  
+        end
+
         new_hash[name][key] << new_value.to_s
+
       end
-    end   
+    end
   end
   new_hash
-end 
-    
+end
